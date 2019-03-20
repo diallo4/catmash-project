1eb0c3d4f4b2d3f2a5ac1c48c8747578b70a09f0
Aboubacar Diallo <aboubacar.diallo-prestataire@laposte.fr>
2019-03-20 14:34:04 +0100

    first commit

---
+++ catmash-frontend/src/pages/chap6/CitizenPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="page">
+        <div id="head">
+            <img src="../../assets/simpsons.png">
+        </div>
+        <div id="content">
+            <citizens></citizens>
+            <citizen></citizen>
+        </div>
+    </div>
+</template>
+
+<script>
+    import citizens from '../../components/chap4/Citizens.vue'
+    import citizen from '../../components/chap4/Citizen.vue'
+
+    export default{
+        components: {citizens, citizen}
+    }
+</script>
